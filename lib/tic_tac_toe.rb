4296fc4d6a9818a9bfb6538b01b5f2f9648c36b1
meaghan jennings <meaghanljennings@gmail.com>
2021-02-17 21:58:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,7 +70,23 @@ def turn(board)
 end
 
 def won?(board)
-  
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index1 = win_combination[0]
+    win_index2 = win_combination[1]
+    win_index3 = win_combination[2]
+    position1 = board[win_index1]
+    position2 = board[win_index2]
+    position3 = board[win_index3]
+    if (position1 == "X" && position2 == "X" && position3 == "X") || (position1 == "O" && position2 == "O" && position3 == "O")
+      return win_combination
+    else
+      #do nothing
+    end
+  end
+  return nil 
+end
+
+
 
 
   
