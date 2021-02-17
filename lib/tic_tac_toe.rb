577dc609c4380c143c722fedda6844dda58b992e
meaghan jennings <meaghanljennings@gmail.com>
2021-02-17 22:52:42 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,13 +1,15 @@
 WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
+ [0,1,2],
+ [3,4,5],
+ [6,7,8],
+ [0,3,6],
+ [1,4,7],
+ [2,5,8],
+ [0,4,8],
+ [2,4,6]
+ ]
+ 
+board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -17,131 +19,123 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
+display_board(board)
+
+def input_to_index(input)
+  input.to_i - 1
 end
 
-def move(board, index, current_player)
-  board[index] = current_player
+def move(board, index, marker)
+  board[index] = marker
 end
 
-def position_taken?(array, index)
-  if (array[index] == " " || array[index] == "" || array[index] == nil)
-    return false
-  else
-    return true
-  end
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
 end
 
-def valid_move?(array, index)
-  if (index < 0 || index > 8)
-    return false
-  elsif position_taken?(array, index)
-    return false
-  else
-    return true
-  end
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-def turn_count(array)
+def turn_count(board)
   counter = 0
-  array.each do |element|
-    if (element == "X" || element == "O")
+  board.each do |element|
+    if element == "X" || element == "O"
       counter += 1
     else
-      # do nothing
+      #do nothing
     end
   end
   return counter
-end
+end 
 
 def current_player(board)
-  if (turn_count(board).even? == true)
+  if turn_count(board).even? == true
     return "X"
   else
     return "O"
   end
 end
 
-def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
-      return win_combination
-    else
-      # do nothing
-    end
+def turn(board)
+  puts "Please enter 1-9:"        # ask for input 
+  input = gets.strip              # get input 
+  index = input_to_index(input)   # convert input to index 
+  if valid_move?(board, index)    # if move is valid,  
+    move(board, index, current_player(board))   #make move for index
+    display_board(board)          # & show board 
+  else                            # else 
+    turn(board)                   # ask input again until valid move
   end
-  return nil
 end
 
-def full?(array)
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index1 = win_combination[0]
+    win_index2 = win_combination[1]
+    win_index3 = win_combination[2]
+    position1 = board[win_index1]
+    position2 = board[win_index2]
+    position3 = board[win_index3]
+    if (position1 == "X" && position2 == "X" && position3 == "X") || (position1 == "O" && position2 == "O" && position3 == "O")
+      return win_combination
+    else
+      #do nothing
+    end
+  end
+  return nil 
+end
+
+def full?(board)
   counter = 0
-  array.each do |element|
-    if (element == "X" || element == "O")
+  board.each do |element|
+    if element == "X" || element == "O"
       counter += 1
     else
       # do nothing
     end
   end
-  if counter == 9
+  if counter == 9 
     return true
   else
     return false
   end
 end
 
-def draw?(array)
-  if full?(array) && !won?(array)
+def draw?(board)
+  if full?(board) && !won?(board)
     return true
   else
     return false
   end
 end
 
-def over?(array)
-  if ( won?(array) || draw?(array) || full?(array) )
+def over?(board)
+  if full?(board) || won?(board) || draw?(board)
     return true
   else
     return false
   end
-end
+end 
 
-def winner(array)
-  if won?(array)
+def winner(board)
+  if won?(board)
     WIN_COMBINATIONS.each do |win_combination|
-      position_1 = array[win_combination[0]]
-      position_2 = array[win_combination[1]]
-      position_3 = array[win_combination[2]]
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
-        return "X"
-      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
-        return "O"
-      else
-        # do nothing
+      position1 = board[win_combination[0]]
+      position2 = board[win_combination[1]]
+      position3 = board[win_combination[2]]
+        if (position1 == "X" && position2 == "X" && position3 == "X")
+          return "X"
+        elsif (position1 == "O" && position2 == "O" && position3 ==  "O")
+          return "O"
+        else
+          #do nothing
+        end
       end
-    end
-  else
+  else 
     return nil
   end
-end
+end 
 
 def play(board)
   while !over?(board)
